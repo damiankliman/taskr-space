ea2e199cd566fef3b843df3fe454a2b1de47b2ce
Damian Kliman <klimandamian@gmail.com>
2022-01-11 14:38:52 -0800

    integrated backend with current task list functions

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,15 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Task({ task, onToggle, onDelete }) {
-    console.log(task)
     return (
         <div className='task-box--rounded'>
-            <input className='check-box--large' type='checkbox' checked={task.complete} onChange={() => onToggle(task.id)}/>
+            <input className='check-box--large' type='checkbox' checked={task.complete} onChange={() => onToggle(task._id, task.complete)}/>
             <div className='task-content'>
-                <div className='task-title'>{task.title}</div>
-                <div className='task-description'>{task.text}</div>
+                <div className='task-title'>{task.taskTitle}</div>
+                <div className='task-description'>{task.taskInfo}</div>
             </div>
-            <FontAwesomeIcon icon={faTrashAlt} style={{color:'#FF0000'}} size='sm' onClick={() => onDelete(task.id)} />
+            <FontAwesomeIcon icon={faTrashAlt} style={{color:'#FF0000'}} size='sm' onClick={() => onDelete(task._id)} />
         </div>
     );
 }
--- src/pages/TaskSpace.js
+++ src/pages/TaskSpace.js
@@ -1,76 +1,115 @@
-import './TaskSpace.css';
+import "./TaskSpace.css";
 
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
-import { faComments } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faComments } from "@fortawesome/free-solid-svg-icons";
 
-import ChatBubble from '../components/ChatBubble';
-import Tasks from '../components/Tasks';
+import ChatBubble from "../components/ChatBubble";
+import Tasks from "../components/Tasks";
 
 function TaskSpacePage() {
-    const [tasks, setTasks] = useState(
-        //for testing only - to be integrated with database
-        [
-            {
-                id: 1,
-                title: 'some title',
-                text: 'some test task',
-                complete: false,
-            },
-            {
-                id: 2,
-                title: 'some other task',
-                text: 'more test text',
-                complete: true,
-            }
-        ]
-    )
+  const [tasks, setTasks] = useState([]);
 
-    const toggleComplete = (id) => {
-        setTasks(
-            tasks.map((task) => 
-                task.id === id ? { ...task, complete: !task.complete } : task
-            )
-        )
+  useEffect(() => {
+    Axios.get("http://dev.lan:3001/read").then((response) => {
+      setTasks(response.data);
+    });
+  }, []);
+
+  const toggleComplete = async (_id, complete) => {
+    try {
+      await Axios.put(`http://dev.lan:3001/complete/${_id}`, {
+        complete: !complete,
+      });
+      Axios.get("http://dev.lan:3001/read").then((response) => {
+        setTasks(response.data);
+      });
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    const toggleDelete = (id) => {
-        setTasks(
-            tasks.filter((task) => task.id !== id)
-        )
-    }
+  const toggleDelete = (_id) => {
+    Axios.delete("http://dev.lan:3001/delete", { data: { _id: _id } });
+    const newTasks = tasks.filter((task) => task._id !== _id);
+    setTasks(newTasks);
+  };
 
-    return (
-        <div className='interface-wrapper'>
-            <div className='header'>
-                <div className='title'>Title</div>
-                <button className='btn--circle'>
-                    <FontAwesomeIcon icon={faUserPlus} style={{color: '#868a92'}} size='lg' />
-                </button>
-            </div>
-            <div className='task-list'>
-                <Tasks tasks={tasks} onToggle={toggleComplete} onDelete={toggleDelete} />
-            </div>
-            <div className='chat-list'>
-                <ChatBubble msgStyle='sent' name='Damian' message='I think this is going great!'/>
-                <ChatBubble msgStyle='recieved' name='Natalie' message='Your code is spaget' />
-                <ChatBubble msgStyle='recieved' name='Natalie' message='Fix this nonsense' />
-                <ChatBubble msgStyle='sent' name='Damian' message='I think this is going great!'/>
-                <ChatBubble msgStyle='recieved' name='Natalie' message='Your code is spaget' />
-                <ChatBubble msgStyle='recieved' name='Natalie' message='Fix this nonsense' />
-                <ChatBubble msgStyle='sent' name='Damian' message='I think this is going great!'/>
-            </div>    
-            <div className='footer'>
-                <input type='text' className='input--rounded' placeholder='Type a message...' />
-                <button className='btn__send--rounded'>
-                    <FontAwesomeIcon icon={faComments} style={{color:'#000'}} size='lg' />
-                </button>
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="interface-wrapper">
+      <div className="header">
+        <div className="title">Title</div>
+        <button className="btn--circle">
+          <FontAwesomeIcon
+            icon={faUserPlus}
+            style={{ color: "#868a92" }}
+            size="lg"
+          />
+        </button>
+      </div>
+      <div className="task-list">
+        <Tasks
+          tasks={tasks}
+          onToggle={toggleComplete}
+          onDelete={toggleDelete}
+        />
+      </div>
+      <div className="chat-list">
+        <ChatBubble
+          msgStyle="sent"
+          name="Damian"
+          message="I think this is going great!"
+        />
+        <ChatBubble
+          msgStyle="recieved"
+          name="Natalie"
+          message="Your code is spaget"
+        />
+        <ChatBubble
+          msgStyle="recieved"
+          name="Natalie"
+          message="Fix this nonsense"
+        />
+        <ChatBubble
+          msgStyle="sent"
+          name="Damian"
+          message="I think this is going great!"
+        />
+        <ChatBubble
+          msgStyle="recieved"
+          name="Natalie"
+          message="Your code is spaget"
+        />
+        <ChatBubble
+          msgStyle="recieved"
+          name="Natalie"
+          message="Fix this nonsense"
+        />
+        <ChatBubble
+          msgStyle="sent"
+          name="Damian"
+          message="I think this is going great!"
+        />
+      </div>
+      <div className="footer">
+        <input
+          type="text"
+          className="input--rounded"
+          placeholder="Type a message..."
+        />
+        <button className="btn__send--rounded">
+          <FontAwesomeIcon
+            icon={faComments}
+            style={{ color: "#000" }}
+            size="lg"
+          />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TaskSpacePage;
