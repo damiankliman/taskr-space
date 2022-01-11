8e6d9a636e7869d9a1b1b5a731b9f83bb69816f2
Damian Kliman <klimandamian@gmail.com>
2022-01-11 15:59:13 -0800

    Add CRUD api endpoints

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,7 +4,7 @@ function Tasks({ tasks, onToggle, onDelete }) {
     return (
         <>
           {tasks.map((task) => (
-              <Task key={task.id} task={task} onToggle={onToggle} onDelete={onDelete} />
+              <Task key={task._id} task={task} onToggle={onToggle} onDelete={onDelete} />
             ))}  
         </>
     )
